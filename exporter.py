1e517e1236eedccac5f7f663360789b798cadd5d
Alvaro Frias Garay <alvarofriasgaray@gmail.com>
2021-03-03 15:11:32 -0300

    Added Exporter's parent class upgrading the abstraction

--- exporter.py
+++ exporter.py
@@ -1,9 +1,22 @@
 import re
 
 
-class Exporter:
+class FileHandler:
     def __init__(self, filepath):
         self.filepath = filepath
+
+    def get_parent_dir(self):
+        return '/'.join((self.filepath).split('/')[:-1])
+
+    def create_lua_filepath(self):
+        par_dir = self.get_parent_dir()
+        lua_name = (self.filepath).split('/')[-1].split('.')[0] + '.lua'
+        return par_dir + '/' + lua_name
+
+
+class Exporter(FileHandler):
+    def __init__(self, filepath):
+        super(Exporter, self).__init__(filepath)
 
     def export_to_lua(self):
         with open(self.filepath, mode='r') as f:
@@ -14,8 +27,7 @@ class Exporter:
         formatted_data = re.split(r'__lua__|__gfx__', cart_data)
         code = formatted_data[1]
 
-        path_lua_file = '/'.join((self.filepath).split('/')[:-1])
-        lua_name = (self.filepath).split('/')[-1].split('.')[0] + '.lua'
+        lua_path = self.create_lua_filepath()
 
-        with open(path_lua_file + '/' + lua_name, mode='w') as new_file:
+        with open(lua_path, mode='w') as new_file:
             new_file.write(code)
