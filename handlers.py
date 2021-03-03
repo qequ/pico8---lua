25fd1efe00805dcdb01563fb04cda541aaa16267
Alvaro Frias Garay <alvarofriasgaray@gmail.com>
2021-03-03 20:27:11 -0300

    Added Importer class

--- handlers.py
+++ handlers.py
@@ -1,4 +1,15 @@
 import re
+from pathlib import Path
+
+
+def map_jumplines(s):
+    if s.startswith('\n'):
+        s = s[1:]
+
+    if not s.endswith('\n'):
+        s += '\n'
+
+    return s
 
 
 class FileHandler:
@@ -33,3 +44,51 @@ class Exporter(FileHandler):
             new_file.write(code)
 
         return ("ok", lua_path)
+
+
+class Importer(FileHandler):
+    def __init__(self, filepath, lua_filepath):
+        super(Importer, self).__init__(filepath)
+        self.lua_filepath = lua_filepath
+
+    def import_to_pico(self):
+        # the code limit is 32k
+        CARTRIDGE_CODE_LIMIT = 32768
+
+        lua_size = Path(self.lua_filepath).stat().st_size
+
+        if lua_size > CARTRIDGE_CODE_LIMIT:
+            return 'fail_codesize'
+
+        with open(self.filepath, mode='r') as cart_file:
+            cart_data = cart_file.read()
+
+        with open(self.lua_filepath, mode='r') as lua_file:
+            lua_code = lua_file.read()
+
+        cart_separators = ['__gfx__', '__gff__',
+                           '__label__', '__map__', '__sfx__', '__music__']
+        split_pattern = '__lua__'
+
+        for cs in cart_separators:
+            if cs in cart_data:
+                split_pattern += '|' + cs
+
+        formatted_data = re.split(split_pattern, cart_data)
+        formatted_data[1] = lua_code
+
+        sep_in_cart = split_pattern.split('|')
+        index_to_insert = 1
+        while len(sep_in_cart) != 0:
+            sep_to_insert = sep_in_cart.pop(0)
+            formatted_data.insert(index_to_insert, sep_to_insert)
+            index_to_insert += 2
+
+        formatted_data = map(map_jumplines, formatted_data)
+
+        ready_to_write_data = ''.join(formatted_data)
+
+        with open(self.filepath + '7', mode='w') as f:
+            f.write(ready_to_write_data)
+
+        return 'ok'
